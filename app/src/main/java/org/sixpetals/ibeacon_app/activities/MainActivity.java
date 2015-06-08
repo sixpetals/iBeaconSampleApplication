e07ab793519d529dca08ad06089d5b90afd65c9b
git -v <takuma.sugimoto@aegif.jp>
2015-06-08 19:06:45 +0900

    initial commit!

---
+++ app/src/main/java/org/sixpetals/ibeacon_app/activities/MainActivity.java
@@ -0,0 +1,131 @@
+package org.sixpetals.ibeacon_app.activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.RemoteException;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
+import org.sixpetals.ibeacon_app.R;
+
+
+public class MainActivity extends ActionBarActivity implements BeaconConsumer {
+    private BeaconManager beaconManager;
+    private BeaconParser beaconParser;
+
+    // iBeaconのデータを認識するためのParserフォーマット
+    public static final String IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+
+        // BeaconParseを設定
+        beaconParser = new BeaconParser();
+        beaconParser.setBeaconLayout(IBEACON_FORMAT);
+
+        beaconManager.getBeaconParsers().clear();
+        beaconManager.getBeaconParsers().add(beaconParser);
+
+    }
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                // 領域への入場を検知
+                Log.d("Beacon", "ENTER Region.");
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                // 領域からの退場を検知
+                Log.d("Beacon", "EXIT Region. ");
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+                // 領域への入退場のステータス変化を検知
+                Log.d("MyActivity", "DetermineState: " + i);
+            }
+        });
+
+        try {
+            // ビーコン情報の監視を開始
+            beaconManager.startMonitoringBeaconsInRegion(new Region("unique-id-001", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        beaconManager.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
---
+++ app/src/main/java/org/sixpetals/ibeacon_app/model/question.java
@@ -0,0 +1,8 @@
+package org.sixpetals.ibeacon_app.model;
+
+/**
+ * Created by takuma.sugimoto on 2015/05/08.
+ */
+public class question {
+
+}
